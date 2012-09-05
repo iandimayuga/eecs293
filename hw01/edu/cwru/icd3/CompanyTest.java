8907f39124fdde6b04294e4cb22f78ea76d11a48
icd3 <git@iandimayuga.com>
2012-09-05 13:37:39 -0400

    Create JUnit Test Case for Company

---
+++ hw01/edu/cwru/icd3/CompanyTest.java
@@ -0,0 +1,89 @@
+/**
+ * Ian Dimayuga
+ * EECS293 HW1
+ */
+package edu.cwru.icd3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ian
+ *
+ */
+public class CompanyTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#Company()}.
+     */
+    @Test
+    public void testCompany() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#add(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public void testAdd() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#addAll(java.util.Set, java.lang.String)}.
+     */
+    @Test
+    public void testAddAll() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#delete(java.lang.String)}.
+     */
+    @Test
+    public void testDelete() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#managerOf(java.lang.String)}.
+     */
+    @Test
+    public void testManagerOf() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#managerSet()}.
+     */
+    @Test
+    public void testManagerSet() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#departmentOf(java.lang.String)}.
+     */
+    @Test
+    public void testDepartmentOf() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link edu.cwru.icd3.Company#managersByDepartmentSize()}.
+     */
+    @Test
+    public void testManagersByDepartmentSize() {
+        fail("Not yet implemented");
+    }
+
+}
