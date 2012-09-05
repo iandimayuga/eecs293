c6cd074c2eb43832115eeea4569db4ec5289ffa4
icd3 <git@iandimayuga.com>
2012-09-05 00:18:41 -0400

    Change tabs to spaces

--- hw01/edu/cwru/icd3/Company.java
+++ hw01/edu/cwru/icd3/Company.java
@@ -14,66 +14,106 @@ import java.util.Set;
  * @author ian A data structure to represent a strictly hierarchical organization of employees.
  */
 public class Company {
-	/**
-	 * Map of employee names to manager names. Each employee has one manager, with the exception of the CEO.
-	 */
-	private Map<String, String> m_managerMap;
+    /**
+     * Map of employee names to manager names. Each employee has one manager, with the exception of the CEO.
+     */
+    private Map<String, String> m_managerMap;
+
+    /**
+     * Map of employee names to the set of direct subordinates.
+     */
+    private Map<String, Set<String>> m_directReportMap;
+
+    /**
+     * Creates an empty Company with no employees.
+     */
+    public Company() {
+        m_managerMap = new HashMap<String, String>();
+        m_directReportMap = new HashMap<String, Set<String>>();
+    }
+
+    /**
+     * Adds an employee to the company, specifying their manager.
+     *
+     * @param employee
+     *            Name of the employee to be added.
+     * @param manager
+     *            Name of an existing employee to be the manager of the new employee. A manager of null is used if and
+     *            only if the Company is currently empty, and the new employee will be the CEO.
+     * @throws NullPointerException
+     *             If employee is null.
+     * @throws IllegalArgumentException
+     *             If the specified manager is null but the Company is not empty.
+     * @throws NoSuchElementException
+     *             If the specified manager is nonexistent.
+     * @throws IllegalArgumentException
+     *             If there is already an employee with that name in the Company.
+     */
+    public void add(String employee, String manager) {
+        // Null-check employee
+        if (employee == null) {
+            throw new NullPointerException("employee cannot be null");
+        }
+        if (manager == null) {
+            // If manager is null, but the Company isn't empty, throw exception
+            if (m_directReportMap.size() > 0) {
+                throw new IllegalArgumentException("manager is null, but Company is not empty.");
+            }
+        } else if (!m_directReportMap.containsKey(manager)) {
+            // If manager doesn't exist in the Company, throw exception
+            // This includes the case where the Company is empty but a manager was specified
+            throw new NoSuchElementException(String.format("Company has no employee with name %s.", manager));
+        }
+        // If employee already exists in the Company, throw exception
+        if (m_managerMap.containsKey(employee)) {
+            throw new IllegalArgumentException(
+                    String.format("Company already contains an employee named %s.", employee));
+        }
+        // Add employee to Company, and to manager's set of direct reports if manager exists
+        if (manager != null) {
+            m_directReportMap.get(manager).add(employee);
+        }
+        m_directReportMap.put(employee, new HashSet<String>());
+        m_managerMap.put(employee, manager);
+    }
 
 	/**
-	 * Map of employee names to the set of direct subordinates.
-	 */
-	private Map<String, Set<String>> m_directReportMap;
-
-	/**
-	 * Creates an empty Company with no employees.
-	 */
-	public Company() {
-		m_managerMap = new HashMap<String, String>();
-		m_directReportMap = new HashMap<String, Set<String>>();
-	}
-
-	/**
-	 * Adds an employee to the company, specifying their manager.
+	 * Removes an employee from the Company, including from any list of direct reports. The employee may not be a
+	 * manager.
 	 * 
 	 * @param employee
-	 *            Name of the employee to be added.
-	 * @param manager
-	 *            Name of an existing employee to be the manager of the new employee. A manager of null is used if and
-	 *            only if the Company is currently empty, and the new employee will be the CEO.
+	 *            Name of the employee to delete.
 	 * @throws NullPointerException
 	 *             If employee is null.
-	 * @throws IllegalArgumentException
-	 *             If the specified manager is null but the Company is not empty.
 	 * @throws NoSuchElementException
-	 *             If the specified manager is nonexistent.
+	 *             If employee is nonexistent.
 	 * @throws IllegalArgumentException
-	 *             If there is already an employee with that name in the Company.
+	 *             If the employee has any direct reports.
 	 */
-	public void add(String employee, String manager) {
+	public void delete(String employee) {
 		// Null-check employee
 		if (employee == null) {
 			throw new NullPointerException("employee cannot be null");
 		}
-		if (manager == null) {
-			// If manager is null, but the Company isn't empty, throw exception
-			if (m_directReportMap.size() > 0) {
-				throw new IllegalArgumentException("manager is null, but Company is not empty.");
-			}
-		} else if (!m_directReportMap.containsKey(manager)) {
-			// If manager doesn't exist in the Company, throw exception
-			// This includes the case where the Company is empty but a manager was specified
-			throw new NoSuchElementException(String.format("Company has no employee with name %s.", manager));
+
+		// Check for existence
+		if (!m_managerMap.containsKey(employee)) {
+			throw new NoSuchElementException(String.format("Company has no employee with name %s.", employee));
 		}
-		// If employee already exists in the Company, throw exception
-		if (m_managerMap.containsKey(employee)) {
-			throw new IllegalArgumentException(
-					String.format("Company already contains an employee named %s.", employee));
+		// Check for direct reports
+		if (m_directReportMap.get(employee).size() > 0) {
+			throw new IllegalArgumentException(String.format("Employee '%s' has direct reports and cannot be deleted.",
+					employee));
 		}
-		// Add employee to Company, and to manager's set of direct reports if manager exists
-		if (manager != null) {
-			m_directReportMap.get(manager).add(employee);
+		
+		String manager = m_managerMap.get(employee);
+		if (manager != null)
+		{
+			// Remove employee from manager's department (unless manager is null)
+			m_directReportMap.get(manager).remove(employee);
 		}
-		m_directReportMap.put(employee, new HashSet<String>());
-		m_managerMap.put(employee, manager);
+		// Delete records of employee
+		m_managerMap.remove(employee);
+		m_directReportMap.remove(employee);
 	}
 }
