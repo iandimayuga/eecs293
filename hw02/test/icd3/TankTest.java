5fdf8a45d29cc3b11dc4ed5369abf923ca40bd93
icd3 <git@iandimayuga.com>
2012-09-12 16:31:27 -0400

    Implement Tank Tests

--- hw02/test/icd3/TankTest.java
+++ hw02/test/icd3/TankTest.java
@@ -14,12 +14,18 @@ import org.junit.Test;
  *
  */
 public class TankTest {
+    private double[] m_bottomLeft;
+    private double[] m_topRight;
 
     /**
      * @throws java.lang.Exception
      */
     @Before
     public void setUp() throws Exception {
+        double[] bottomLeft = { -0.5, 1, -4.5 };
+        double[] topRight = { 0.5, 2, 3.5 };
+        m_bottomLeft = bottomLeft;
+        m_topRight = topRight;
     }
 
     /**
@@ -27,47 +33,59 @@ public class TankTest {
      */
     @Test
     public void testTank() {
-        fail("Not yet implemented");
+        Tank empty = new Tank();
+        assertTrue(empty.getBottom() == 0.0);
+        assertTrue(empty.getTop() == 0.0);
+        assertTrue(empty.baseArea() == 0.0);
     }
 
     /**
-     * Test method for {@link icd3.Tank#Tank(double[], double[])}.
+     * Test method for Illegal State behavior of {@link icd3.Tank#setCoordinates(double[], double[])}.
      */
-    @Test
-    public void testTankDoubleArrayDoubleArray() {
-        fail("Not yet implemented");
+    @Test(expected=IllegalStateException.class)
+    public void testSetCoordinatesIllegalState() {
+        Tank tank = new Tank();
+        tank.setCoordinates(new double[3], new double[3]);
+        tank.setCoordinates(new double[3], new double[3]);
     }
 
     /**
-     * Test method for {@link icd3.Tank#setCoordinates(double[], double[])}.
+     * Test method for negative coordinate behavior of {@link icd3.Tank#setCoordinates(double[], double[])}.
      */
-    @Test
-    public void testSetCoordinates() {
-        fail("Not yet implemented");
+    @Test(expected=IllegalArgumentException.class)
+    public void testSetCoordinatesNegative() {
+        Tank tank = new Tank();
+        tank.setCoordinates(m_topRight, m_bottomLeft);
     }
 
     /**
-     * Test method for {@link icd3.Tank#getBottom()}.
+     * Test method for not enough min dimensions behavior of {@link icd3.Tank#setCoordinates(double[], double[])}.
      */
-    @Test
-    public void testGetBottom() {
-        fail("Not yet implemented");
+    @Test(expected=IllegalArgumentException.class)
+    public void testSetCoordinatesWrongMinDimension() {
+        Tank tank = new Tank();
+        tank.setCoordinates(new double[2], new double[3]);
     }
 
     /**
-     * Test method for {@link icd3.Tank#getTop()}.
+     * Test method for not enough max dimensions behavior of {@link icd3.Tank#setCoordinates(double[], double[])}.
      */
-    @Test
-    public void testGetTop() {
-        fail("Not yet implemented");
+    @Test(expected=IllegalArgumentException.class)
+    public void testSetCoordinatesWrongMaxDimension() {
+        Tank tank = new Tank();
+        tank.setCoordinates(new double[3], new double[2]);
     }
 
     /**
-     * Test method for {@link icd3.Tank#baseArea()}.
+     * Test method for {@link icd3.Tank}.
      */
     @Test
-    public void testBaseArea() {
-        fail("Not yet implemented");
+    public void testTankBehavior() {
+        Tank tank = new Tank();
+        tank.setCoordinates(m_bottomLeft, m_topRight);
+        assertTrue(tank.getBottom() == -4.5);
+        assertTrue(tank.getTop() == 3.5);
+        assertTrue(tank.baseArea() == 1.0);
     }
 
 }
