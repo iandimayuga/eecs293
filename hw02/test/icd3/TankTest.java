9c15b1f874fb0a97c55eafbfb32f44c1a4f9835c
icd3 <git@iandimayuga.com>
2012-09-12 15:21:03 -0400

    Add test case for Tank

---
+++ hw02/test/icd3/TankTest.java
@@ -0,0 +1,73 @@
+/**
+ * Ian Dimayuga
+ * EECS293 HW02
+ */
+package icd3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ian
+ *
+ */
+public class TankTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link icd3.Tank#Tank()}.
+     */
+    @Test
+    public void testTank() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.Tank#Tank(double[], double[])}.
+     */
+    @Test
+    public void testTankDoubleArrayDoubleArray() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.Tank#setCoordinates(double[], double[])}.
+     */
+    @Test
+    public void testSetCoordinates() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.Tank#getBottom()}.
+     */
+    @Test
+    public void testGetBottom() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.Tank#getTop()}.
+     */
+    @Test
+    public void testGetTop() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.Tank#baseArea()}.
+     */
+    @Test
+    public void testBaseArea() {
+        fail("Not yet implemented");
+    }
+
+}
