effd876a0bfcad0cee4eb8a8722a5e0cdce3431d
icd3 <git@iandimayuga.com>
2012-09-13 04:46:14 -0400

    Add WaterSystemTest class

---
+++ hw02/test/icd3/WaterSystemTest.java
@@ -0,0 +1,63 @@
+/**
+ * Ian Dimayuga
+ * EECS293 HW02
+ */
+package icd3;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author ian
+ *
+ */
+public class WaterSystemTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public void setUpBeforeClass() throws Exception
+    {
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#tanksByBottom()}.
+     */
+    @Test
+    public void testTanksByBottom()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#tanksByTop()}.
+     */
+    @Test
+    public void testTanksByTop()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#activeTanks()}.
+     */
+    @Test
+    public void testActiveTanks()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#activeBaseArea()}.
+     */
+    @Test
+    public void testActiveBaseArea()
+    {
+        fail("Not yet implemented");
+    }
+
+}
