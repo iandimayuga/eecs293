6b70f9dd94a9f2349b221669fe781512fc3dc940
icd3 <git@iandimayuga.com>
2012-09-13 17:04:02 -0400

    Remove unnecessary import in WaterSystemTest

--- hw02/test/icd3/WaterSystemTest.java
+++ hw02/test/icd3/WaterSystemTest.java
@@ -4,8 +4,8 @@
  */
 package icd3;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.NavigableMap;
