8764f9ef56ff80c7c7d68e793a626bac082f3ef5
icd3 <git@iandimayuga.com>
2012-09-27 14:03:15 -0400

    Add HW04 copied from HW03

---
+++ hw04/src/icd3/WaterSystem.java
@@ -0,0 +1,278 @@
+/**
+ * Ian Dimayuga
+ * EECS293 HW04
+ */
+package icd3;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * A data structure to represent a system of Tanks related by their heights and vertical positions.
+ *
+ * @author ian
+ *
+ */
+public class WaterSystem
+{
+
+    /**
+     * The raw set of tanks in this system.
+     */
+    private Set<Tank> m_tankSet;
+
+    /**
+     * A map that associates to each break point the set of tanks whose bottoms are exactly at the break point.
+     */
+    private NavigableMap<Double, Set<Tank>> m_tanksByBottom;
+
+    /**
+     * A map that associates to each break point the set of tanks whose tops are exactly at the break point.
+     */
+    private NavigableMap<Double, Set<Tank>> m_tanksByTop;
+
+    /**
+     * A map that associates to each break point the set of tanks active at the break point.
+     */
+    private NavigableMap<Double, Set<Tank>> m_activeTanks;
+
+    /**
+     * A map that associates to each break point the total active base area at the break point.
+     */
+    private NavigableMap<Double, Double> m_activeBaseArea;
+
+    /**
+     * Initializes the WaterSystem to the specified set of Tanks.
+     *
+     * @param initialSystem The set of Tanks to represent the WaterSystem.
+     */
+    public WaterSystem(Set<Tank> initialSystem)
+    {
+        // Store the raw set
+        this.setTankSystem(initialSystem);
+    }
+
+    /**
+     * Sets the system to the specified set of tanks.
+     * @param tankSystem The set of Tanks representing the WaterSystem.
+     */
+    public void setTankSystem(Set<Tank> tankSystem)
+    {
+        m_tankSet = new HashSet<>(tankSystem);
+
+        // Invalidate the cache
+        m_tanksByBottom = m_tanksByTop = m_activeTanks = null;
+        m_activeBaseArea = null;
+    }
+
+    /**
+     * Get a NavigableMap associating bottom edges with sets of tanks.
+     *
+     * @return The desired Map.
+     */
+    public NavigableMap<Double, Set<Tank>> tanksByBottom()
+    {
+        // If the cache is invalid, then regenerate the requested map
+        if (null == m_tanksByBottom) {
+            m_tanksByBottom = generateTanksByEdge(m_tankSet, false);
+        }
+        return m_tanksByBottom;
+    }
+
+    /**
+     * Get a NavigableMap associating top edges with sets of tanks.
+     *
+     * @return The desired Map.
+     */
+    public NavigableMap<Double, Set<Tank>> tanksByTop()
+    {
+        // If the cache is invalid, then regenerate the requested map
+        if (null == m_tanksByTop) {
+            m_tanksByTop = generateTanksByEdge(m_tankSet, true);
+        }
+        return m_tanksByTop;
+    }
+
+    /**
+     * Get a NavigableMap associating breakpoints to sets of active tanks.
+     *
+     * @return The desired Map.
+     */
+    public NavigableMap<Double, Set<Tank>> activeTanks()
+    {
+        // If the cache is invalid, then regenerate the requested map
+        if (null == m_activeTanks) {
+            // Call dependencies which can either return cached structures or regenerate them in turn
+            m_activeTanks = generateActiveTanks(this.tanksByBottom(), this.tanksByTop());
+        }
+        return m_activeTanks;
+    }
+
+    /**
+     * Get a NavigableMap associating breakpoints to total active cross-section area.
+     *
+     * @return The desired Map.
+     */
+    public NavigableMap<Double, Double> activeBaseArea()
+    {
+        // If the cache is invalid, then regenerate the requested map
+        if (null == m_activeBaseArea) {
+            // Call dependency which can either return cached structure or regenerate it in turn
+            m_activeBaseArea = generateActiveBaseArea(this.activeTanks());
+        }
+        return m_activeBaseArea;
+    }
+
+    /**
+     * Generate a NavigableMap of breakpoints to sets of tanks. Breakpoints can either be the tops or bottoms.
+     *
+     * @param tankSet The set of Tanks in the WaterSystem.
+     * @param top true if the Map should associate top edges with tanks, false if it should associate bottom edges.
+     * @return The desired Map.
+     */
+    private static NavigableMap<Double, Set<Tank>> generateTanksByEdge(Set<Tank> tankSet, boolean top)
+    {
+        // Initialize the map to be returned
+        NavigableMap<Double, Set<Tank>> tanksByEdge = new TreeMap<>();
+
+        // Add each tank to the map with its top or bottom height as the key
+        for (Tank tank : tankSet)
+        {
+            double edge = top ? tank.getTop() : tank.getBottom();
+
+            // Create a new Set if the breakpoint is not yet recognized
+            if (!tanksByEdge.containsKey(edge))
+            {
+                tanksByEdge.put(edge, new HashSet<Tank>());
+            }
+
+            // Add the Tank to the Set
+            tanksByEdge.get(edge).add(tank);
+        }
+
+        return tanksByEdge;
+    }
+
+    /**
+     * Generate a NavigableMap of breakpoints to sets of active tanks.
+     *
+     * @param tanksByBottom A NavigableMap of bottom edges to sets of tanks.
+     * @param tanksByTop A NavigableMap of top edges to sets of tanks.
+     * @return The desired Map.
+     */
+    private static NavigableMap<Double, Set<Tank>> generateActiveTanks(NavigableMap<Double, Set<Tank>> tanksByBottom,
+                                                                       NavigableMap<Double, Set<Tank>> tanksByTop)
+    {
+        // Initialize the map to be returned
+        NavigableMap<Double, Set<Tank>> activeTanks = new TreeMap<>();
+
+        if (tanksByBottom.size() == 0) {
+            return activeTanks;
+        }
+
+        // Create a running Set of active tanks
+        Set<Tank> runningActiveTanks = new HashSet<>();
+
+        // Get the EntrySet Iterators from both tanksByBottom and tanksByTop
+        Iterator<Entry<Double, Set<Tank>>> bottomsFromBelow = tanksByBottom.entrySet().iterator();
+        Iterator<Entry<Double, Set<Tank>>> topsFromBelow = tanksByTop.entrySet().iterator();
+
+        // Current top and bottom points
+        Entry<Double, Set<Tank>> currentBottom = bottomsFromBelow.next();
+        Entry<Double, Set<Tank>> currentTop = topsFromBelow.next();
+
+        // Don't advance the bottom break point if there is nothing left
+        boolean bottomFinished = false, topFinished = false;
+
+        // Iterate through both entry sets in parallel, activating tanks as they are encountered in bottoms,
+        // and deactivating them as they are encountered in tops
+        // The last bottom can never be at or above the last top, so this condition is sufficient
+        while (!topFinished)
+        {
+            // This will be set to either the bottom or the top, or both (if equal)
+            double currentBreakPoint = 0;
+
+            // Hold onto the top break point so both ifs are checking the same value
+            double top = currentTop.getKey();
+
+            // The lower current key is the current break point to consider
+            // Both conditionals evaluate to true if bottom and top are equal, so they both would advance
+            if (bottomFinished || top <= currentBottom.getKey())
+            {
+                currentBreakPoint = top;
+
+                // This is a top break point, so deactivate
+                runningActiveTanks.removeAll(currentTop.getValue());
+
+                // Then advance the current top key, unless top is finished
+                if (topsFromBelow.hasNext())
+                {
+                    currentTop = topsFromBelow.next();
+                }
+                else
+                {
+                    topFinished = true;
+                }
+            }
+            if (!bottomFinished && currentBottom.getKey() <= top)
+            {
+                currentBreakPoint = currentBottom.getKey();
+
+                // This is a bottom break point, so activate
+                runningActiveTanks.addAll(currentBottom.getValue());
+
+                // Then advance the current bottom key, unless bottom is finished
+                if (bottomsFromBelow.hasNext())
+                {
+                    currentBottom = bottomsFromBelow.next();
+                }
+                else
+                {
+                    bottomFinished = true;
+                }
+            }
+
+            // Add the entry
+            activeTanks.put(currentBreakPoint, new HashSet<Tank>(runningActiveTanks));
+        }
+        assert(bottomFinished);
+
+        return activeTanks;
+    }
+
+    /**
+     * Generate a NavigableMap of breakpoints to the total base area of the active tanks.
+     *
+     * @param activeTanks A NavigableMap associating breakpoints to sets of active tanks.
+     * @return The desired Map.
+     */
+    private static NavigableMap<Double, Double> generateActiveBaseArea(NavigableMap<Double, Set<Tank>> activeTanks)
+    {
+        // Initialize the map to be returned
+        NavigableMap<Double, Double> activeBaseArea = new TreeMap<>();
+
+        // Iterate through the break points and sum the areas at each height
+        for (double height : activeTanks.navigableKeySet())
+        {
+
+            // Get active tanks at height
+            Set<Tank> tanks = activeTanks.get(height);
+
+            // Get the total base area
+            double totalArea = 0;
+            for (Tank tank : tanks)
+            {
+                totalArea += tank.baseArea();
+            }
+
+            // Create the entry for this break point
+            activeBaseArea.put(height, totalArea);
+        }
+
+        return activeBaseArea;
+    }
+}
---
+++ hw04/test/icd3/WaterSystemTest.java
@@ -0,0 +1,142 @@
+/**
+ * Ian Dimayuga
+ * EECS293 HW04
+ */
+package icd3;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.NavigableMap;
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author ian
+ *
+ */
+public class WaterSystemTest
+{
+    private static Tank s_tankA;
+    private static Tank s_tankB;
+    private static Tank s_tankC;
+    private static Tank s_tankD;
+    private static WaterSystem s_testSystem;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        // Create the test case example from the assignment
+        double[] bottomA = { 0, 0, 5 };
+        double[] topA = { 5, 1, 13 };
+        double[] bottomB = { 0, 0, 11 };
+        double[] topB = { 5, 1, 18 };
+        double[] bottomC = { 0, 0, 15 };
+        double[] topC = { 4, 1, 21 };
+        double[] bottomD = { 0, 0, 19 };
+        double[] topD = { 8, 1, 23 };
+
+        s_tankA = new Tank(bottomA, topA);
+        s_tankB = new Tank(bottomB, topB);
+        s_tankC = new Tank(bottomC, topC);
+        s_tankD = new Tank(bottomD, topD);
+
+        Set<Tank> tankSet = new HashSet<Tank>();
+        tankSet.add(s_tankA);
+        tankSet.add(s_tankB);
+        tankSet.add(s_tankC);
+        tankSet.add(s_tankD);
+
+        s_testSystem = new WaterSystem(tankSet);
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#tanksByBottom()}.
+     */
+    @Test
+    public void testTanksByBottom()
+    {
+        NavigableMap<Double, Set<Tank>> tanks = s_testSystem.tanksByBottom();
+        assertTrue(tanks.get(5.0).size() == 1);
+        assertTrue(tanks.get(5.0).contains(s_tankA));
+        assertFalse(tanks.containsKey(13.0));
+        assertTrue(tanks.get(11.0).size() == 1);
+        assertTrue(tanks.get(11.0).contains(s_tankB));
+        assertFalse(tanks.containsKey(18.0));
+        assertTrue(tanks.get(15.0).size() == 1);
+        assertTrue(tanks.get(15.0).contains(s_tankC));
+        assertFalse(tanks.containsKey(21.0));
+        assertTrue(tanks.get(19.0).size() == 1);
+        assertTrue(tanks.get(19.0).contains(s_tankD));
+        assertFalse(tanks.containsKey(23.0));
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#tanksByTop()}.
+     */
+    @Test
+    public void testTanksByTop()
+    {
+        NavigableMap<Double, Set<Tank>> tanks = s_testSystem.tanksByTop();
+        assertTrue(tanks.get(13.0).size() == 1);
+        assertTrue(tanks.get(13.0).contains(s_tankA));
+        assertFalse(tanks.containsKey(5.0));
+        assertTrue(tanks.get(18.0).size() == 1);
+        assertTrue(tanks.get(18.0).contains(s_tankB));
+        assertFalse(tanks.containsKey(11.0));
+        assertTrue(tanks.get(21.0).size() == 1);
+        assertTrue(tanks.get(21.0).contains(s_tankC));
+        assertFalse(tanks.containsKey(15.0));
+        assertTrue(tanks.get(23.0).size() == 1);
+        assertTrue(tanks.get(23.0).contains(s_tankD));
+        assertFalse(tanks.containsKey(19.0));
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#activeTanks()}.
+     */
+    @Test
+    public void testActiveTanks()
+    {
+        NavigableMap<Double, Set<Tank>> tanks = s_testSystem.activeTanks();
+        assertTrue(tanks.get(5.0).size() == 1);
+        assertTrue(tanks.get(5.0).contains(s_tankA));
+        assertTrue(tanks.get(11.0).size() == 2);
+        assertTrue(tanks.get(11.0).contains(s_tankA) && tanks.get(11.0).contains(s_tankB));
+        assertTrue(tanks.get(13.0).size() == 1);
+        assertTrue(tanks.get(13.0).contains(s_tankB));
+        assertTrue(tanks.get(15.0).size() == 2);
+        assertTrue(tanks.get(15.0).contains(s_tankB) && tanks.get(15.0).contains(s_tankC));
+        assertTrue(tanks.get(18.0).size() == 1);
+        assertTrue(tanks.get(18.0).contains(s_tankC));
+        assertTrue(tanks.get(19.0).size() == 2);
+        assertTrue(tanks.get(19.0).contains(s_tankC) && tanks.get(19.0).contains(s_tankD));
+        assertTrue(tanks.get(21.0).size() == 1);
+        assertTrue(tanks.get(21.0).contains(s_tankD));
+        assertTrue(tanks.get(23.0).size() == 0);
+    }
+
+    /**
+     * Test method for {@link icd3.WaterSystem#activeBaseArea()}.
+     */
+    @Test
+    public void testActiveBaseArea()
+    {
+        NavigableMap<Double, Double> tanks = s_testSystem.activeBaseArea();
+        assertTrue(tanks.get(5.0) == 5.0);
+        assertTrue(tanks.get(11.0) == 10.0);
+        assertTrue(tanks.get(13.0) == 5.0);
+        assertTrue(tanks.get(15.0) == 9.0);
+        assertTrue(tanks.get(18.0) == 4.0);
+        assertTrue(tanks.get(19.0) == 12.0);
+        assertTrue(tanks.get(21.0) == 8.0);
+        assertTrue(tanks.get(23.0) == 0.0);
+    }
+
+}
